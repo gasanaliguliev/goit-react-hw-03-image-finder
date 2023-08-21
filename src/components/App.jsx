ca85e311bf75b010190e53ca935b95c100f32404
gasanaliguliev <aligulievgasan001@gmail.com>
2023-08-22 01:59:14 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import axios from 'axios';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+import './styles.css';
+
+
+export class App extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    showModal: false,
+    selectedImage: null,
+    isLoading: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchImages();
+    }
+  }
+
+  fetchImages = () => {
+    const { query, page } = this.state;
+    this.setState({ isLoading: true });
+
+    const apiKey = '38971521-301f5cb08025e3967497dc80d';
+    axios
+      .get(
+        `https://pixabay.com/api/?key=${apiKey}&q=${query}&page=${page}&per_page=12`
+      )
+      .then((response) => {
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...response.data.hits],
+          page: prevState.page + 1,
+        }));
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      });
+  };
+
+  handleSearchSubmit = (query) => {
+    this.setState({ query, images: [], page: 1 });
+  };
+
+  handleImageClick = (selectedImage) => {
+    this.setState({ selectedImage, showModal: true });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ selectedImage: null, showModal: false });
+  };
+
+  render() {
+    const { images, isLoading, showModal, selectedImage } = this.state;
+
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+        <ImageGallery images={images} onItemClick={this.handleImageClick} />
+        {isLoading && <Loader />}
+        {images.length > 0 && !isLoading && (
+          <Button onClick={this.fetchImages} />
+        )}
+        {showModal && (
+          <Modal image={selectedImage} onClose={this.handleCloseModal} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
+
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { ReactComponent as Icon } from '../icon/search.svg';
+
+class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ query: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.query);
+  };
+
+  render() {
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <button type="submit" className="SearchForm-button">
+            <Icon width={20} height={20}/>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
